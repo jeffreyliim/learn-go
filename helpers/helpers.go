908207801d0a8f42fa70139cda2d805ade4b7ec6
Jeffrey Lim <jeffrey_lim95@hotmail.com>
2019-07-07 23:57:29 +0800

    added more helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -17,3 +17,57 @@ func indexOf(val string, ss []string) int {
 	}
 	return -1 //not found.
 }
+
+func hasOwnProperty(m map[string]string, p string) bool {
+	for i := range m {
+		if i == p {
+			return true
+		}
+	}
+	return false
+}
+
+//	push to back of array
+func push(arr []string, str string) []string {
+	arr = append(arr, str)
+	return arr
+}
+
+//	Push to front of array
+func unshift(arr []string, str string) []string {
+	arr = append([]string{str}, arr...)
+	return arr
+}
+
+//	remove front of array
+func shift(arr []string, str string) []string {
+	if len(arr) < 1 {
+		arr = arr[:0]
+	} else {
+		arr = arr[1:]
+	}
+	return arr
+}
+
+// remove from back of array
+func pop(arr []string, str string) []string {
+	if len(arr) < 1 {
+		arr = arr[:0]
+	} else {
+		arr = arr[0 : len(arr)-1]
+	}
+	return arr
+}
+
+// remove duplicates in array
+func unique(intSlice []int) []int {
+	keys := make(map[int]bool)
+	list := []int{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	arr := []int{1, 2, 3, 4}
-	Solution(arr, 4)
+
+	Solution([]int{0, 1, 1, 0})
 }
 
-func Solution(A []int, K int) []int {
+func Solution(A []int) int {
+	counter := 0
 
-	A = filter(A, func(s int) bool {
-		return s < 3
-	})
-	fmt.Print(A)
-	return A
-}
-
-func filter(strArr []int, test func(int) bool) (ret []int) {
-	for _, element := range strArr {
-		if test(element) {
-			ret = append(ret, element)
+	for i := range A {
+		if i < len(A)-1 {
+			if A[i] == A[i+1] {
+				counter++
+				if A[len(A)-1] != A[len(A)-2] {
+					counter++
+				}
+			}
 		}
 	}
-	return ret
+	return counter
 }
